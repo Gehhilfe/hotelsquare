b3f2ff6348817e8d3c7ef292eb7eb3f64d161fc8
Tim Burkert <burkert.tim@gmail.com>
2017-05-04 21:55:34 +0200

    Add tests to increase code coverage

--- test/util.js
+++ test/util.js
@@ -1,3 +1,26 @@
-/**
- * Created by gehhi on 04.05.2017.
- */
+const Util = require('../lib/util');
+const mongoose = require('mongoose');
+const chai = require('chai');
+const expect = chai.expect;
+
+
+describe('util', function () {
+    describe('connectDatabase', () => {
+        it('should connect to a database', (done) => {
+            Util.connectDatabase(mongoose).then(() => {
+                expect(mongoose.connection.db).to.not.be.undefined;
+                return done();
+            });
+        });
+
+        it('should connect to a database only once', (done) => {
+            Util.connectDatabase(mongoose).then(() => {
+                var db = mongoose.connection.db;
+                Util.connectDatabase(mongoose).then(() => {
+                    expect(mongoose.connection.db).to.be.equal(db);
+                    return done();
+                });
+            });
+        });
+    });
+});
