b5c0c3d4dd35e5523c634a2a14a4ef7e33cc5308
Tim Burkert <burkert.tim@gmail.com>
2017-05-06 23:42:41 +0200

    Add database bootstrap when not in production env

--- backend/test/util.js
+++ backend/test/util.js
@@ -2,9 +2,46 @@ const Util = require('../lib/util');
 const mongoose = require('mongoose');
 const chai = require('chai');
 const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
+const User = require('../app/models/user');
 
 describe('util', function () {
+
+    describe('bootstrap', () => {
+        beforeEach((done) => {
+            mongoose.Promise = global.Promise;
+
+            if (mongoose.connection.db) return done();
+            Util.connectDatabase(mongoose).then(function () {
+                User.remove({}, done);
+            });
+        });
+
+        it('should reject on null', () => {
+            return expect(Util.bootstrap(User, null)).eventually.rejected;
+        });
+
+        it('should reject on undefined', () => {
+            return expect(Util.bootstrap(User, undefined)).eventually.rejected;
+        });
+
+        it('should reject on non array', () => {
+            return expect(Util.bootstrap(User, {a: 'b'})).eventually.rejected;
+        });
+
+        it('should resolve', () => {
+            return expect(Util.bootstrap(User, [
+                {
+                    name: 'test',
+                    email: 'test@test.de',
+                    password: 'admin1'
+                }
+            ])).eventually.fulfilled;
+        });
+    });
+
     describe('connectDatabase', () => {
         it('should connect to a database', (done) => {
             Util.connectDatabase(mongoose).then(() => {
