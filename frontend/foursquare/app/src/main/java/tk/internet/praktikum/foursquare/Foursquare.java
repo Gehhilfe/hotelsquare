6e1400914e8ad85d40ba496bfe32b948d2b8584d
truongtud <truongtud@googlemail.com>
2017-05-11 15:53:00 +0200

    initial foursquare

---
+++ frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/Foursquare.java
@@ -0,0 +1,13 @@
+package tk.internet.praktikum.foursquare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Foursquare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foursquare);
+    }
+}
