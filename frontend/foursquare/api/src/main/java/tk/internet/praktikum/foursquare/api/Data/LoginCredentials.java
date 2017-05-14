6f8de70d94f17a19c41a547fb78b2ea24832a3c3
Robert Königstein <robert.koenigstein@t-online.de>
2017-05-14 17:25:48 +0200

    login made nearly possible from backend, first java wrapper library init

---
+++ frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/Data/LoginCredentials.java
@@ -0,0 +1,13 @@
+package tk.internet.praktikum.foursquare.api.Data;
+
+/**
+ * Created by robert on 14.05.2017.
+ */
+
+public class LoginCredentials {
+
+    public String name;
+
+    public String password;
+
+}
---
+++ frontend/foursquare/api/src/test/java/tk/internet/praktikum/foursquare/api/ExampleUnitTest.java
@@ -0,0 +1,28 @@
+package tk.internet.praktikum.foursquare.api;
+
+import org.junit.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.AsyncSubject;
+import tk.internet.praktikum.foursquare.api.Data.LoginCredentials;
+import tk.internet.praktikum.foursquare.api.Data.TokenInformation;
+import tk.internet.praktikum.foursquare.api.Interfaces.SessionInterface;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void login_working() throws Exception {
+
+
+    }
+}
