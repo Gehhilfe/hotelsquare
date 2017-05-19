c6dfe948b0d564f405241b6e2c0d6b82dbe38be3
Tim Burkert <burkert.tim@gmail.com>
2017-05-19 22:20:46 +0200

    Cleanup

--- backend/server.js
+++ backend/server.js
@@ -31,30 +31,12 @@ server.use(restify.bodyParser({mapParams: true}));
 
 server.pre(require('./app/middleware/log'));
 
-/*
-server.pre(function (req, res, next) {
-    console.log('filtering server request');
+// session
+server.post('session', session.postSession);
 
-    const filters = [{route: /^\/user.*$/g, method: 'DELETE'}];
-    let filters_passed = true;
-    filters.forEach((e) => {
-        if (req.url.match(e.route) !== null && req.method === e.method) {
-            if (req.headers['x-auth'] === undefined) {
-                res.status(403);
-                res.json({error: 'authentication failed'});
-                filters_passed = false;
-            }
-
-        }
-    });
-    if (filters_passed)
-        return next();
-});*/
-
-server.post('/session', session.postSession);
-
-server.post('/user', user.postUser);
-server.del('/user', auth, user.deleteUser);
+// user
+server.post('user', user.postUser);
+server.del('user', auth, user.deleteUser);
 
 server.listen(8081, function () {
     console.log('%s listening at %s', server.name, server.url);
