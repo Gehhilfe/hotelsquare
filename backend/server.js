b5b00a2f548a560f3c087c15fa91e6e45c469324
Tim Burkert <burkert.tim@gmail.com>
2017-05-19 18:53:53 +0200

    Fix lint error in server.js

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,11 @@ const server = restify.createServer();
 
 util.connectDatabase(mongoose).then(() => {
     //Bootstrap database
-    if(process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== 'production') {
         const User = require('./app/models/user');
 
-        if(config.bootstrap) {
-            if(config.bootstrap.User)
+        if (config.bootstrap) {
+            if (config.bootstrap.User)
                 util.bootstrap(User, config.bootstrap.User);
         }
     }
@@ -27,28 +27,26 @@ util.connectDatabase(mongoose).then(() => {
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-server.use(restify.bodyParser({ mapParams: true }));
+server.use(restify.bodyParser({mapParams: true}));
 
 server.pre(require('./app/middleware/log'));
 
 server.pre(function (req, res, next) {
-   console.log('filtering server request');
+    console.log('filtering server request');
 
-   const filters = [{route : /^\/user.*$/g, method: 'DELETE'}];
-   let filters_passed = true;
-   filters.forEach((e)=>
-    {
-       if(req.url.match(e.route) !== null && req.method === e.method)
-       {
-           if(req.headers["x-auth"] === undefined)
-           {
-               res.status(403);
-               res.json({error: 'authentication failed'});
-               filters_passed = false;
-           }
+    const filters = [{route: /^\/user.*$/g, method: 'DELETE'}];
+    let filters_passed = true;
+    filters.forEach((e) => {
+        if (req.url.match(e.route) !== null && req.method === e.method) {
+            if (req.headers['x-auth'] === undefined) {
+                res.status(403);
+                res.json({error: 'authentication failed'});
+                filters_passed = false;
+            }
 
-    }});
-    if(filters_passed)
+        }
+    });
+    if (filters_passed)
         return next();
 });
 
@@ -56,7 +54,7 @@ server.post('/session', session.postSession);
 
 server.post('/user', user.postUser);
 
-server.listen(8081, function() {
+server.listen(8081, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
 
