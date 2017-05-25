7954405b796bc3de9eff8f75c8e6c72165f5a7da
Tim Burkert <burkert.tim@gmail.com>
2017-05-25 14:48:38 +0200

    User name is now unique

--- backend/app/models/user.js
+++ backend/app/models/user.js
@@ -15,6 +15,7 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         minlength: 4,
+        unique: true,
         match: [/^[a-zA-Z][a-zA-Z0-9-_]*$/, 'Only a-z,A-Z,0-9,-,_ characters are allowed as name']
     },
     email: {
