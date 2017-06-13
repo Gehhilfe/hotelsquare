c114d24fcf85fd826ae5efb70f355a993b458455
Christian <christian.hack2@googlemail.com>
2017-06-14 00:47:54 +0200

    Converted LoginActivity to LoginFragment

--- frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/login/LoginFragment.java
+++ frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/login/LoginFragment.java
@@ -1,27 +1,28 @@
-package tk.internet.praktikum.foursquare;
+package tk.internet.praktikum.foursquare.login;
 
+import android.app.Fragment;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import tk.internet.praktikum.foursquare.R;
 import tk.internet.praktikum.foursquare.api.ServiceFactory;
 import tk.internet.praktikum.foursquare.api.bean.LoginCredentials;
 import tk.internet.praktikum.foursquare.api.service.SessionService;
 import tk.internet.praktikum.foursquare.api.service.UserService;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginFragment extends Fragment {
 
-    private static final String LOG_TAG = LoginActivity.class.getSimpleName();
-    private static final int REGISTER_REQUEST = 0;    // Register Request Tag für switching Between the Register and Login Activity
-    private static final int RESTORE_PW_REQUEST = 1;
+    private static final String LOG_TAG = LoginFragment.class.getSimpleName();
     private final String URL = "https://dev.ip.stimi.ovh/";
 
     private EditText userInput, passwordInput;
@@ -29,20 +30,21 @@ public class LoginActivity extends AppCompatActivity {
     private TextView registerLbl, passwordForgottenLbl;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        userInput = (EditText) findViewById(R.id.user_input);
-        passwordInput = (EditText) findViewById(R.id.register_password_input);
-        loginBtn = (AppCompatButton) findViewById(R.id.login_btn);
-        registerLbl = (TextView) findViewById(R.id.login_link);
-        passwordForgottenLbl = (TextView) findViewById(R.id.forgotten_password);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        userInput = (EditText) view.findViewById(R.id.user_input);
+        passwordInput = (EditText) view.findViewById(R.id.register_password_input);
+        loginBtn = (AppCompatButton) view.findViewById(R.id.login_btn);
+        registerLbl = (TextView) view.findViewById(R.id.login_link);
+        passwordForgottenLbl = (TextView) view.findViewById(R.id.forgotten_password);
 
         loginBtn.setOnClickListener(v -> login());
         registerLbl.setOnClickListener(v -> register());
         passwordForgottenLbl.setOnClickListener(v -> restorePassword());
+
+        return view;
     }
+
 
     /**
      * Starts the login sequence. For now it only validates the input and displays the Progress dialog for 3 seconds.
@@ -50,7 +52,7 @@ public class LoginActivity extends AppCompatActivity {
     private void login() {
         loginBtn.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, 0);
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity(), 0);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Waiting for login...");
         progressDialog.show();
@@ -74,7 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                             UserService uservice = ServiceFactory.createRetrofitService(UserService.class, URL, tokenInformation.getToken());
                             successfulLogin();
                             progressDialog.dismiss();
-                            Toast.makeText(getApplicationContext(), tokenInformation.getToken(), Toast.LENGTH_LONG).show();
+                            Toast.makeText(getActivity().getApplicationContext(), tokenInformation.getToken(), Toast.LENGTH_LONG).show();
                         },
                         throwable -> {
                             failedLogin();
@@ -91,8 +93,7 @@ public class LoginActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "Successful login.");
         loginBtn.setEnabled(true);
 
-        finish();
-        // TODO - Return to the FourSquareActivity.
+        getActivity().finish();
     }
 
     /**
@@ -101,40 +102,18 @@ public class LoginActivity extends AppCompatActivity {
     private void failedLogin() {
         Log.d(LOG_TAG, "Failed login.");
         loginBtn.setEnabled(true);
-        Toast.makeText(getBaseContext(), "Failed to login.", Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity().getBaseContext(), "Failed to login.", Toast.LENGTH_LONG).show();
     }
 
     /**
      * Starts the Register Activity.
      */
     private void register() {
-        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-        startActivityForResult(intent, REGISTER_REQUEST);
+        ((LoginActivity) getActivity()).changeFragment(1);
     }
 
     // TODO - Restore Password
     private void restorePassword() {
-        Intent intent = new Intent(getApplicationContext(), RestorePasswordActivity.class);
-        startActivityForResult(intent, RESTORE_PW_REQUEST);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REGISTER_REQUEST :
-                if (resultCode == RESULT_OK) {
-                    // TODO - Start the new Activity after a successful registration. Probably going on the the User Setting view.
-                    // TODO - Or wait at the login activity for an email confirmation and then login.
-                    break;
-                } else
-                    break;
-
-            case RESTORE_PW_REQUEST :
-                if (resultCode == RESULT_OK) {
-                    // TODO - Wait at the login activity for the user to restore his password and login.
-                    break;
-                } else
-                    break;
-        }
+        ((LoginActivity) getActivity()).changeFragment(2);
     }
 }
