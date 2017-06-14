9edc86c224f728465b5f2495fa66c00f4cc0ad6e
Marco Huber <marco.huber95@gmx.de>
2017-06-14 20:31:47 +0200

    Added LocationService. Not really working yet.

--- frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/MapsActivity.java
+++ frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/MapsActivity.java
@@ -1,9 +1,18 @@
 package tk.internet.praktikum.foursquare;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -13,6 +22,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationService locationService = new LocationService();
+    boolean bound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Intent intent = new Intent(this, LocationService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(bound){
+            unbindService(connection);
+            bound = false;
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -36,11 +62,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
+       mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+      ///  LatLng pos = new LatLng(locationService.getLastLocation().getLatitude(), locationService.getLastLocation().getLongitude());
+       // mMap.addMarker(new MarkerOptions().position(pos).title("Here AM I "));
+
     }
+
+    /**
+     * Requests Permissions on Runtime
+     * @return true if Permissions were set on runtime
+     */
+    private boolean runtime_permissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 10);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            LocationService.LocalBinder binder = (LocationService.LocalBinder) service;
+            locationService = binder.getService();
+            Log.d("TAG", "Bon" + locationService);
+            bound = true;
+            // Add a marker in Sydney and move the camera
+            //LatLng sydney = new LatLng(-34, 151);
+            //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            LatLng pos = new LatLng(locationService.getLastLocation().getLatitude(), locationService.getLastLocation().getLongitude());
+            mMap.addMarker(new MarkerOptions().position(pos).title("Lat: " + pos.latitude + " lon: " + pos.longitude));
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bound = false;
+        }
+    };
 }
