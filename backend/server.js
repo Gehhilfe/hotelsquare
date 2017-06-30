2c2ff1f00d0d2e6033501809313cb6e202bb3221
Robert Königstein <robert.koenigstein@t-online.de>
2017-06-30 16:03:06 +0200

    string singlequote lint

--- backend/server.js
+++ backend/server.js
@@ -112,7 +112,7 @@ server.get('user/:name', user.profile);
 server.get('user/:name/avatar', auth, user.getAvatar);
 
 server.post('user', user.register, (request, response, next) => {
-    io.sockets.emit('new user', "hello");
+    io.sockets.emit('new user', 'hello');
 });
 server.post('user/:name/friend_requests', auth, user.sendFriendRequest);
 
