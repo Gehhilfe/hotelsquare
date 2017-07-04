4eb11771ac094c8ae644fc8f1db4ec84ba1bae26
Marco Huber <marco.huber95@gmx.de>
2017-07-04 20:50:20 +0200

    Current State of MapView...

--- frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/LocationTracker.java
+++ frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/LocationTracker.java
@@ -1,78 +1,135 @@
 package tk.internet.praktikum.foursquare;
 
-import android.*;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.common.GoogleApiAvailability;
+
+import org.greenrobot.eventbus.EventBus;
 
 /**
- * Created by Marco Huber on 14.06.2017.
+ * tracks the Location
  */
 
-public class LocationTracker implements LocationListener,
-        ConnectionCallbacks,
-        OnConnectionFailedListener{
+public class LocationTracker implements
+        LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
-    private static final long INT = 1000 * 15;
-    private static final long FASTEST_INT = 1000 *  10;
+    // TAG
+    private final static String TAG = LocationTracker.class.getSimpleName();
+
+    // INTERVALS TODO: SET
+    private static final long INTERVAL = 10 * 45;
+    private static final long FASTEST_INTERVAL = 10 * 30;
+
+    //Variables
     private Context context;
-    private GoogleApiClient googleApiClient;
-    private LocationRequest locRequest;
-    private Location currentLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+
+    // LocationEvent class
+    public static class LocationEvent {
+        public Location location;
+
+        public LocationEvent(Location location) {
+            this.location = location;
+        }
+    }
 
     public LocationTracker(Context context) {
         this.context = context;
-
         init();
     }
 
+    // Setup ApiClient
     private void init() {
-       // if (!GoogleApiAvailability.isGooglePlayServicesAvailable(context)) return;
+        if (!isGooglePlayServicesAvailable(context)) return;
 
-        this.googleApiClient = new GoogleApiClient.Builder(context)
+        this.mGoogleApiClient = new GoogleApiClient.Builder(context)
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
     }
 
+    // Start and Connect
+    public void start() {
+        if (mGoogleApiClient != null) {
+            mLocationRequest = new LocationRequest();
+            mLocationRequest.setInterval(INTERVAL);
+            mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+            mGoogleApiClient.connect();
+        }
+    }
+
+    // Stop and Disconnect
+    public void stop() {
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+            mGoogleApiClient.disconnect();
+            Log.d(TAG, "Location::stop");
+        }
+    }
+
     @Override
     public void onConnected(Bundle bundle) {
-        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            // Permissions missing
+        // check Permissions
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG, "Location tracking::Permission is missing");
+            Log.i(TAG, "Pls grant location permission: Settings > Apps > <This App> > Location");
             return;
         }
-        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locRequest, this);
+
+        // Start Location tracking
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        Log.d(TAG, "Location::start");
     }
 
-    @Override
-    public void onConnectionSuspended(int i) {
-        // Nothing to do
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        // Connection Failed
-    }
-
+    // On ChangeListener
     @Override
     public void onLocationChanged(Location location) {
-        currentLocation = location;
-        // TODO: pass
+        // set new Location
+        mCurrentLocation = location;
+        Log.d(TAG, "Location("+location.getLatitude()+","+location.getLongitude()+")");
+        // Post to Bus
+        EventBus.getDefault().post(new LocationEvent(mCurrentLocation));
+    }
+
+    // Nothing to Do
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    // On Connection Failed
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection failed: " + connectionResult.toString());
+    }
+
+    // Check if Available
+    public static boolean isGooglePlayServicesAvailable(Context context) {
+        // deprecated, but no way to fix due to version issues?
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/MapsActivity.java
+++ frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/MapsActivity.java
@@ -1,20 +1,26 @@
 package tk.internet.praktikum.foursquare;
 
+import android.content.Intent;
 import android.location.Location;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
     private Marker yourMarker;
@@ -24,44 +30,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * On Map Ready
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // What to do?
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
+        // set Map
+        // TODO: set current
+        map = googleMap;
+        // Just to test
+        //map.addMarker(new MarkerOptions().position(new LatLng(49.877050, 8.654878)).title("Office"));
+       //map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(49.877050, 8.654878), 17));
     }
 
-    /**
-     * Updates your position on the map
-     * @param location
-     */
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-    private void updateMap(Location location){
-        if(yourMarker == null) {
-            yourMarker = map.addMarker(
-                    new MarkerOptions()
-                            .position(new LatLng(location.getLatitude(), location.getLongitude()))
-                            .title("Your Position"));
-        }else{
-                yourMarker.setPosition(new LatLng( location.getLatitude(), location.getLongitude()));
+        Intent mIntent = new Intent(this, LocationService.class);
+        startService(mIntent);
+
+        // off-topic -> ignore this
+        //EventBus.getDefault().register(this);
+    }
+
+    public void updateMap(Location location){
+        if(yourMarker == null){
+            yourMarker = map.addMarker(new MarkerOptions()
+            .position(new LatLng(location.getLatitude(),location.getLongitude()))
+            .title("Your Position"));
+            }else{
+                yourMarker.setPosition((new LatLng(location.getLatitude(),location.getLongitude())));
             }
-            map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
         }
 
     //TODO
-    private void updateFriendsMarker(Location[] locations){
+    public void updateFriendsmarker(Location[] locations){
 
     }
 
     //TODO
-    private void setVenuesMarker(Location location){
+    public void updateVenuesMarker(Location[] locations){
 
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Intent mIntent = new Intent(this, LocationService.class);
+        stopService(mIntent); // stop tracking service
+
+        // off-topic -> ignore this
+        //EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * Listen for new database entries from background service
+     * @param event
+     */
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(LocationTracker.LocationEvent event) {
+        Location location = event.location;
+        updateMap(location);
+    }
+
+
 }
