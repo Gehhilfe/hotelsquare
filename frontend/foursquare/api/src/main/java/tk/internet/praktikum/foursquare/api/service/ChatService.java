76fc1e0fc0a9664eb114bc1e54a067542c357bbd
Tim Burkert <burkert.tim@gmail.com>
2017-07-05 08:27:16 +0200

    Change chat REST routes

--- frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/service/ChatService.java
+++ frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/service/ChatService.java
@@ -9,20 +9,22 @@ import io.socket.client.Socket;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import tk.internet.praktikum.foursquare.api.bean.Chat;
 import tk.internet.praktikum.foursquare.api.bean.Message;
+import tk.internet.praktikum.foursquare.api.bean.RecipientMessage;
 
 public interface ChatService {
 
     /**
      * Pushes a chat to a dedicated recipient
      *
-     * @param recipients receiver of the message of the newly generated Chat
+     * @param message First message including all recipients
      * @return chatId and status message ("New Chat")
      */
-    @POST("chat/{recipients}")
-    Observable<Object> newChat(@Path("recipients") String recipients, @Body Message message);
+    @POST("chats")
+    Observable<Object> newChat(@Body RecipientMessage message);
 
     /**
      * Replies to an old message
@@ -30,7 +32,7 @@ public interface ChatService {
      * @param chatId id of chat, where the message is published
      * @return status message ("replied to message")
      */
-    @POST("chat/reply/{chatId}")
+    @POST("chats/{chatId}/messages")
     Observable<Object> replyMessage(@Path("chatId") String chatId, @Body Message message);
 
     /**
@@ -39,7 +41,7 @@ public interface ChatService {
      * @param chatId id of chat
      * @return chat history of passed ID
      */
-    @GET("chat/with/{chatId}")
+    @GET("chats/{chatId}")
     Observable<Chat> getConversation(@Path("chatId") String chatId);
 
     /**
@@ -47,6 +49,6 @@ public interface ChatService {
      *
      * @return all chats with the last message as teaser (you can get more if you want... ;)) for the authenticated user
      */
-    @GET("chat/all")
+    @GET("chats")
     Observable<List<Chat>> getConversations();
 }
