5a3051bacea36537f0ac8f37c01d384fe725ba7a
Tim Burkert <burkert.tim@gmail.com>
2017-07-05 15:30:45 +0200

    Add pagination support for veneu search query for frontend api

--- frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/service/VenueService.java
+++ frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/service/VenueService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import io.reactivex.Observable;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import tk.internet.praktikum.foursquare.api.bean.Venue;
 import tk.internet.praktikum.foursquare.api.bean.VenueSearchQuery;
 import tk.internet.praktikum.foursquare.api.bean.VenueSearchResult;
@@ -12,11 +13,18 @@ import tk.internet.praktikum.foursquare.api.bean.VenueSearchResult;
 public interface VenueService {
 
     /**
-     * Retrieves venues for current request
+     * Retrieves venues fitting a search query.
      *
      * @return venues for requested parameters
      */
+    @POST("searches/venues/{page}")
+    Observable<VenueSearchResult> queryVenue(@Body VenueSearchQuery query, @Path("page") int page);
+
+    /**
+     * Retrieves venues fitting a search query.
+     *
+     * @return Search result fitting the query
+     */
     @POST("searches/venues")
     Observable<VenueSearchResult> queryVenue(@Body VenueSearchQuery query);
-
 }
