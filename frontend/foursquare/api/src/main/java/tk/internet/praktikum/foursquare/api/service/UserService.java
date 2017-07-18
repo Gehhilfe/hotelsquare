264155f967d61799cab47d746ed43bc26bc01a8f
Tim Burkert <burkert.tim@gmail.com>
2017-07-18 20:46:30 +0200

    Add route for friend list

--- frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/service/UserService.java
+++ frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/service/UserService.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import retrofit2.http.Body;
-import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import tk.internet.praktikum.foursquare.api.bean.FriendListResponse;
 import tk.internet.praktikum.foursquare.api.bean.User;
 import tk.internet.praktikum.foursquare.api.bean.UserSearchQuery;
 
@@ -66,4 +66,12 @@ public interface UserService {
      */
     @POST("searches/users")
     Observable<List<User>> search(@Body UserSearchQuery query);
+
+    /**
+     * Retrvies friends list for user
+     * @param page number page starts with 0
+     * @return
+     */
+    @GET("profile/friends/{page}")
+    Observable<FriendListResponse> friends(@Path("page") int page);
 }
