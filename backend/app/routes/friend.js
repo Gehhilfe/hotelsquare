ba6d79a36a0eb40a5138dd1741db37932e2f7b69
Robert Königstein <robert.koenigstein@t-online.de>
2017-07-19 15:34:01 +0200

    get nearby friends route added

--- backend/app/routes/friend.js
+++ backend/app/routes/friend.js
@@ -4,7 +4,13 @@ const _ = require('lodash');
 const restify = require('restify');
 const User = require('../models/user');
 
-
+/**
+ * returns a bunch of 20 friends
+ * @param request request
+ * @param response response
+ * @param next next
+ * @returns {undefined}
+ */
 async function getFriends(request, response, next) {
     let page = 0;
     if (request.params.page)
@@ -24,8 +30,34 @@ async function getFriends(request, response, next) {
         count: friends,
         friends: _.map(user.friends, (it) => it.toJSONPublic())
     });
+    return next();
+}
+
+/**
+ * returns all friends within 5km radius
+ * @param request request
+ * @param response response
+ * @param next next
+ * @returns {undefined}
+ */
+async function getNearByFriends(request, response, next) {
+    let radius = 5000;
+    let user = await User.findOne({
+        _id: request.authentication._id
+    });
+
+    let users = await User.find({
+        _id : { $in : user.friends },
+        incognito: false
+    }).where('location').near({
+        center: request.body,
+        maxDistance: radius
+    });
+    response.send(_.map(user, (it) => it.toJSONPublic()));
+    return next();
 }
 
 module.exports = {
-    getFriends
+    getFriends,
+    getNearByFriends
 };
