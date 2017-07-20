f3ae8299fdf347832a30fdd082577c984245e0e2
Robert Königstein <robert.koenigstein@t-online.de>
2017-07-20 13:39:08 +0200

    user bean + getter setter gender, - setter mail / name

--- frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/bean/User.java
+++ frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/bean/User.java
@@ -43,10 +43,6 @@ public class User implements Serializable {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
@@ -57,10 +53,6 @@ public class User implements Serializable {
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDisplayName() {
@@ -112,5 +104,13 @@ public class User implements Serializable {
 
     public void setIncognito(boolean incognito) {
         this.incognito = incognito;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
     }
 }
