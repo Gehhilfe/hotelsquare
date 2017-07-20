b091f28d45e98b341b146bec527e0820e0f6f2a6
Robert Königstein <robert.koenigstein@t-online.de>
2017-07-20 13:40:43 +0200

    und name mail wieder rein für registrierung

--- frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/bean/User.java
+++ frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/bean/User.java
@@ -43,6 +43,10 @@ public class User implements Serializable {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -53,6 +57,10 @@ public class User implements Serializable {
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDisplayName() {
