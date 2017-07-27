9be5f6da68c56c02c73ff089a560f8827c17e44c
Tim Burkert <burkert.tim@gmail.com>
2017-07-27 15:09:26 +0200

    Fix addTextComment timeout

--- backend/app/routes/comment.js
+++ backend/app/routes/comment.js
@@ -55,15 +55,14 @@ function textComment(model) {
     return async (request, response, next) => {
         const [o, author] = await Promise.all([
             model.findOne({_id: request.params.id}),
-            User.findOne({_id: request.authentication._id})
+            User.findOne({_id: request.authentication._id}).populate('avatar')
         ]);
 
         if(!author || !o) {
             return next(restify.errors.BadRequestError('Author or target model not found!'));
         }
 
-        let textComment = await TextComment.build(author, request.body.text, o);
-        textComment = await textComment.populate('author').execPopulate();
+        const textComment = await TextComment.build(author, request.body.text, o);
         await o.save();
         response.send(textComment.toJSONDetails());
         return next();
@@ -79,15 +78,14 @@ function imageComment(model) {
     return async (request, response, next) => {
         const [o, author] = await Promise.all([
             model.findOne({_id: request.params.id}),
-            User.findOne({_id: request.authentication._id})
+            User.findOne({_id: request.authentication._id}).populate('avatar')
         ]);
 
         if(!author || !o) {
             return next(restify.errors.BadRequestError('Author or target model not found!'));
         }
 
-        let imageComment = await ImageComment.build(author, request.files.image.path, o);
-        imageComment = await imageComment.populate('author').execPopulate();
+        const imageComment = await ImageComment.build(author, request.files.image.path, o);
         await o.save();
         response.send(imageComment.toJSONDetails());
         return next();
