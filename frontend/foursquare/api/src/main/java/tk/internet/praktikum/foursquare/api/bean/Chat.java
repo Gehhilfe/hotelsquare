6e8f60708f09559e1af3a077d649db87724a428e
Tim Burkert <burkert.tim@gmail.com>
2017-08-01 19:24:27 +0000

    Update Chat.java

--- frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/bean/Chat.java
+++ frontend/foursquare/api/src/main/java/tk/internet/praktikum/foursquare/api/bean/Chat.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Chat {
 
-    @@SerializedName("_id")
+    @SerializedName("_id")
     private String chatId;
     private List<User> participants;
     private List<ChatMessage> messages;
