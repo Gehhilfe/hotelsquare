a0eff85513fb8ca06e8c83f2e8302fc93afbcfce
Christian <christian.hack2@googlemail.com>
2017-08-01 12:18:59 +0200

    fixed a bug in fragment_profile.xml and added changed dummy activity for testing chat stuff.

--- frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/friendlist/DummyActivity.java
+++ frontend/foursquare/app/src/main/java/tk/internet/praktikum/foursquare/friendlist/DummyActivity.java
@@ -2,19 +2,106 @@ package tk.internet.praktikum.foursquare.friendlist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import tk.internet.praktikum.Constants;
 import tk.internet.praktikum.foursquare.R;
+import tk.internet.praktikum.foursquare.api.ImageCacheLoader;
+import tk.internet.praktikum.foursquare.api.ImageSize;
+import tk.internet.praktikum.foursquare.api.ServiceFactory;
+import tk.internet.praktikum.foursquare.api.bean.Gender;
+import tk.internet.praktikum.foursquare.api.bean.Message;
+import tk.internet.praktikum.foursquare.api.bean.RecipientMessage;
+import tk.internet.praktikum.foursquare.api.service.ChatService;
+import tk.internet.praktikum.foursquare.api.service.ProfileService;
+import tk.internet.praktikum.foursquare.storage.LocalStorage;
 
 public class DummyActivity extends AppCompatActivity {
+    TextView startId, loadId;
+    Button startBtn, loadBtn;
+    private final String URL = "https://dev.ip.stimi.ovh/";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        addFragment();
+        setContentView(R.layout.dummy_activity);
+        
+        startId = (TextView) findViewById(R.id.start_id);
+        loadId = (TextView) findViewById(R.id.load_id);
+
+        startBtn = (Button) findViewById(R.id.start_chat);
+        loadBtn = (Button) findViewById(R.id.load_chat);
+        
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startChat();
+            }
+        });
+        
+        loadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadChat();
+            }
+        });
     }
 
-    public void addFragment() {
-        //getFragmentManager().beginTransaction().add(R.id.login_layout, new FriendListFragment()).commit();
+    private void startChat() {
+        ChatService service = ServiceFactory
+                .createRetrofitService(ChatService.class, URL, LocalStorage.
+                        getSharedPreferences(getApplicationContext()).getString(Constants.TOKEN, ""));
+
+        List<String> recipitants = new ArrayList<>();
+        recipitants.add("597e2a61d0f960001443dbc8");
+        RecipientMessage rm = new RecipientMessage(recipitants, "Test Chat mit Janus");
+
+        try {
+            service.newChat(rm)
+                    .subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            user -> {
+                                Log.d("CHAT DBG", "data here");
+                            },
+                            throwable -> {
+                                Toast.makeText(getApplicationContext(), throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                    );
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    private void loadChat() {
+        ChatService service = ServiceFactory
+                .createRetrofitService(ChatService.class, URL, LocalStorage.
+                        getSharedPreferences(getApplicationContext()).getString(Constants.TOKEN, ""));
+
+        try {
+            service.getConversations()
+                    .subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            user -> {
+                                Log.d("CHAT DBG", "data here");
+                            },
+                            throwable -> {
+                                Toast.makeText(getApplicationContext(), throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                    );
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
